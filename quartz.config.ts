79df174d60591a43d9b3b101fe5e46c87acdcc09
Kam Wing Yiu <ctom3560@gmail.com>
2026-01-31 20:03:16 +0800

    update baseURL

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "rw-systems.ctom3560.workers.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
